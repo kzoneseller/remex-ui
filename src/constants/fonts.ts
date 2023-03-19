bc1a6b2a43f9f6b95525380081205cb78fafda67
Danah <danah.kim@kakao.com>
2023-03-19 17:51:12 +0900

    fix: 폰트 강제 적용 처리

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -2,51 +2,51 @@ import { css } from '@emotion/react';
 
 const fonts = {
   h1: css`
-    font-size: 2rem;
-    font-weight: bold;
+    font-size: 2rem !important;
+    font-weight: bold !important;
   `,
   h2: css`
-    font-size: 1.5rem;
-    font-weight: bold;
+    font-size: 1.5rem !important;
+    font-weight: bold !important;
   `,
   h3: css`
-    font-size: 1.25rem;
-    font-weight: bold;
+    font-size: 1.25rem !important;
+    font-weight: bold !important;
   `,
   h4: css`
-    font-size: 1.125rem;
-    font-weight: bold;
+    font-size: 1.125rem !important;
+    font-weight: bold !important;
   `,
   h5: css`
-    font-size: 1rem;
-    font-weight: bold;
+    font-size: 1rem !important;
+    font-weight: bold !important;
   `,
   h6: css`
-    font-size: 0.875rem;
-    font-weight: bold;
+    font-size: 0.875rem !important;
+    font-weight: bold !important;
   `,
   h7: css`
-    font-size: 0.8125rem;
-    font-weight: bold;
+    font-size: 0.8125rem !important;
+    font-weight: bold !important;
   `,
   p1: css`
-    font-size: 1rem;
+    font-size: 1rem !important;
   `,
   p2: css`
-    font-size: 0.9375rem;
+    font-size: 0.9375rem !important;
   `,
   p3: css`
-    font-size: 0.875rem;
+    font-size: 0.875rem !important;
   `,
   p3Medium: css`
-    font-size: 0.875rem;
-    font-weight: 500;
+    font-size: 0.875rem !important;
+    font-weight: 500 !important;
   `,
   p4: css`
-    font-size: 0.8125rem;
+    font-size: 0.8125rem !important;
   `,
   p5: css`
-    font-size: 0.75rem;
+    font-size: 0.75rem !important;
   `,
 };
 
