2c9c08010fc1d292c4ae00b54a17705f377d166f
hahagarden <tingyuan413@gmail.com>
2024-01-09 08:04:46 +0900

    fix: TableUnpagedLayout 컴포넌트 export 추가

--- src/components/Templates/index.ts
+++ src/components/Templates/index.ts
@@ -1,3 +1,4 @@
 export { default as HeroLayout } from './HeroLayout';
 export { default as StepLayout } from './StepLayout';
 export { default as TableLayout } from './TableLayout';
+export { default as TableUnpagedLayout } from './TableUnpagedLayout';
