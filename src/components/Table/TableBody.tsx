615a917b5bd7be22ad07b2141c655245543c05e9
hahagarden <tingyuan413@gmail.com>
2024-12-02 18:30:45 +0900

    fix: TableBody skeleton 관련 수정

--- src/components/Table/TableBody.tsx
+++ src/components/Table/TableBody.tsx
@@ -1,30 +1,22 @@
 import clsx from 'clsx';
 import { TableSkeleton } from 'components/Skeleton';
-import { Children, ComponentType, forwardRef, HTMLAttributes, isValidElement, PropsWithChildren, useMemo } from 'react';
+import { forwardRef, HTMLAttributes, PropsWithChildren } from 'react';
 import type { CustomStyle } from 'utils/theme';
 
 import { StyledTableBody } from './table.styles';
 
-interface TableBodyProps extends HTMLAttributes<HTMLTableSectionElement> {
+type TableBodyLoadingType = { isLoading?: false } | { isLoading: true; column: number };
+
+type TableBodyProps = HTMLAttributes<HTMLTableSectionElement> & {
   customStyle?: CustomStyle;
-  isLoading?: boolean;
-  column?: number;
   row?: number;
-}
+} & TableBodyLoadingType;
 
 const TableBody = forwardRef<HTMLTableSectionElement, PropsWithChildren<TableBodyProps>>(
-  ({ children, isLoading = false, column, row, customStyle, className, ...props }, ref) => {
-    const columnsByChildren = useMemo(() => {
-      const tableRowComp = Children.toArray(children).find(
-        child => isValidElement(child) && (child.type as ComponentType).displayName === 'TableRow'
-      );
-
-      return isValidElement(tableRowComp) ? Children.count(tableRowComp.props.children) : 1;
-    }, [children]);
-
+  ({ children, isLoading = false, row, customStyle, className, ...props }, ref) => {
     return (
       <StyledTableBody ref={ref} css={customStyle} className={clsx('tbody', className)} {...props}>
-        {isLoading ? <TableSkeleton column={column ?? columnsByChildren} row={row} /> : children}
+        {isLoading ? <TableSkeleton column={(props as { column: number }).column} row={row} /> : children}
       </StyledTableBody>
     );
   }
